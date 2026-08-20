ea25e1c9fbd23df75c01e49e5fd80d79f2ff157a
Nepovezlo <sasasuharevskij98@gmail.com>
2026-08-21 01:55:08 +0300

    Изменить BookingClient

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -38,8 +38,8 @@ public class BookingClient extends BaseClient {
     }
 
     public ResponseEntity<Object> approveBooking(long userId, long bookingId, boolean approved) {
-        Map<String, Object> body = Map.of("approved", approved);
-        return patch("/" + bookingId, userId, body);
+        Map<String, Object> parameters = Map.of("approved", approved);
+        return patch("/" + bookingId, userId, parameters);
     }
 
     public ResponseEntity<Object> bookItem(long userId, BookItemRequestDto requestDto) {
