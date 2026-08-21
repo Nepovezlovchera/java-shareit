c0df4dc4f59632339f5d4ed4b36efdf449043e6c
Nepovezlo <sasasuharevskij98@gmail.com>
2026-08-22 01:47:10 +0300

    Checkstyle

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.booking.dto;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user.dto;
 
-import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
