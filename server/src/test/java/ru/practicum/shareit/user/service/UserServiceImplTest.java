d6a95493accc81f968dedcf3f3e79803ef866591
Nepovezlo <sasasuharevskij98@gmail.com>
2026-08-22 01:36:29 +0300

    Вернуть валидацию

--- server/src/test/java/ru/practicum/shareit/user/service/UserServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/user/service/UserServiceImplTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.exception.DuplicateEmailException;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.model.User;
 
